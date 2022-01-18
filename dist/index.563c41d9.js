7065282ade13ed26221edfb3a7bab9646271c750
powellian <ianpowell01@gmail.com>
2022-01-18 19:13:42 +0000

    1st build adding ./ which isn't working. going down to flat dir

---
+++ dist/index.563c41d9.js
@@ -0,0 +1,2 @@
+console.log("%c Whup? ","background: #00ff00; border: 1px solid #ff00ff");let gradElements=document.querySelectorAll(".gradient-block .pigr__pod");for(let e=0;e<gradElements.length;e++)gradElements[e].classList.add(`gradient-block__inner--0${e}`);
+//# sourceMappingURL=index.563c41d9.js.map
