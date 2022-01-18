db0ffd7f21a0ef8c05116af80fbc8328f8710cae
powellian <ianpowell01@gmail.com>
2022-01-18 19:06:38 +0000

    imports scss & js working

---
+++ dist/index.355035eb.js
@@ -0,0 +1,6 @@
+console.log('%c Whup? ', 'background: #00ff00; border: 1px solid #ff00ff');
+// import 'js/pi-modules/index.js'
+let gradElements = document.querySelectorAll('.gradient-block .pigr__pod');
+for(let i = 0; i < gradElements.length; i++)gradElements[i].classList.add(`gradient-block__inner--0${i}`);
+
+//# sourceMappingURL=index.355035eb.js.map
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// import * as theGradients from 'gradient-block'
+// theGradients.default()
+
+let gradElements = document.querySelectorAll('.gradient-block .pigr__pod')
+for (let i = 0; i < gradElements.length; i++) {
+  gradElements[i].classList.add(`gradient-block__inner--0${i}`)
+}
